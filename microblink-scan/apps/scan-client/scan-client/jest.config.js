1102ae9c42fe923f70483d2d2d9c6b8f96065b0b
Matija Stepanic <stepanic@italk.hr>
2019-01-11 10:29:40 +0100

    Generate app scan-client

---
+++ microblink-scan/apps/scan-client/scan-client/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'scan-client-scan-client',
+  preset: '../../../jest.config.js',
+  coverageDirectory: '../../../coverage/apps/scan-client/scan-client/'
+};
---
+++ microblink-scan/apps/scan-client/scan-client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'microblink-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'scan-client-scan-client';
+}
