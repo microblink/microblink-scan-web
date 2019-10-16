ae33930f4800b0cf71ed547f8fb691b29bbd880a
Rok Stjepanjek <rok.stjepanjek@microblink.com>
2019-10-16 13:50:47 +0200

    Add gulp task documentation, fix firebase storage rules configuration, change version of microblink-js to depend on

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
- 
+
+// https://github.com/angular/angular/issues/21191
+// This gulp task enables the progress event for AJAX requests to propagate to our code instead of getting caught in Angular service worker handling code. 
+
 gulp.task('swAjaxUploadFix', done => {
   gulp.src(['dist/apps/public/scan-client/ngsw-worker.js'])
     .pipe(replace("onFetch(event) {", "onFetch(event) { if (event.request.url.indexOf('/recognize/execute') !== -1) { return; }"))
